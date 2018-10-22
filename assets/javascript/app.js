ff340ca1f86b9eadaa0b895c4c30766b8cb45f31
Dirk Kiesewetter <dirk.k1@outlook.com>
2018-10-22 19:11:50 -0400

    updated files

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,16 +1,13 @@
 console.log("GifTastic app.js loaded")
 
 var topics = [
-    'spaceship', 'X-Wing', 'TIE fighter', 'Death Star', 'B-Wing', 'Star Destroyer', 'astronomy', 'jet'
+    'spaceship', 'Star Wars', 'Star Trek', 'Death Star', 'Stargate', 'Star Destroyer', 'astronomy', 'warbird'
 ]
-
-// var queryURL = "http://api.giphy.com/v1/gifs/search?q=" + (#gif-input) + "&api_key=ZhBYk7Ogg5uqcY86VNUVcMn8UCUesZ9B&limit=10&rating=pg";
 
 // xhr.done(function (data) {
 //     console.log("success got data", data);
 // });
 // My API key is:   ZhBYk7Ogg5uqcY86VNUVcMn8UCUesZ9B
-
 
 //CODE FROM GIPHY:
 
@@ -28,7 +25,35 @@ var topics = [
 
 // var gif = $(this).attr();
 
-// Function for displaying movie data
+function displayGifInfo() {
+
+    var gif = $(this).attr("data-name");
+    var queryURL = "http://api.giphy.com/v1/gifs/search?q=" + "#gif-input" + "&api_key=ZhBYk7Ogg5uqcY86VNUVcMn8UCUesZ9B&limit=10&rating=pg";
+
+    $.ajax({
+        url: queryURL,
+        method: "GET"
+    }).then(function (response) {
+
+        // Creates a div to hold the gif
+        var gifView = $("#gif-view");
+        // make this empty to wipe the screen
+        $("#gif-view").prepend(gifView);
+        console.log(response);
+        var rating = response.Rated;
+        // Retrieves the Rating Data and creates an element to have the rating displayed
+        gifView.append($("<p>").text("Rating: " + rating));
+        
+        // Creates an element to hold the image
+        // Appends the image
+        gifView.append($("<img>").attr("src", response.Poster));
+
+        // Puts the entire Movie above the previous movies.
+    });
+
+}
+
+// Function for displaying data
 function renderButtons() {
 
     // Deletes the movies prior to adding new movies
@@ -37,7 +62,7 @@ function renderButtons() {
     // Loops through the array of movies
     for (var i = 0; i < topics.length; i++) {
 
-        // Then dynamicaly generates buttons for each movie in the array
+        // Then dynamically generates buttons for each movie in the array
         //This code $("<button>") is all jQuery needs to create the beginning and end tag. (<button></button>)
         var a = $("<button>");
         // Adds a class of movie to our button
@@ -51,8 +76,23 @@ function renderButtons() {
     }
 }
 
-function importGifs() {
 
-}
+// This function handles events where the add movie button is clicked
+$("#add-gif").on("click", function (event) {
+    event.preventDefault();
+    // This line of code will grab the input from the textbox
+    var gif = $("#gif-input").val().trim();
+
+    // The movie from the textbox is then added to our array
+    topics.push(gif);
+
+    // Calling renderButtons which handles the processing of our movie array
+    renderButtons();
+});
+
+// Adding click event listeners to all elements with a class of "movie"
+$(document).on("click", ".gif", displayGifInfo);
+
+
 
 renderButtons();
