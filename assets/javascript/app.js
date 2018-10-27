c6f99be11af84bcf3aaaa7421590435972eb0192
Dirk Kiesewetter <dirk.k1@outlook.com>
2018-10-27 08:24:46 -0400

    updated files

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,109 +1,151 @@
-console.log("GifTastic app.js loaded")
-
-var gifView;
-var staticGif;
-var movingGif;
-var dataState;
-
-var topics = [
-    'Battlestar Galactica', 'Star Wars', 'Star Trek', 'Peter Sellers', 'Stargate', 'galaxies', 'solar system', 'jet fighter'
-]
-
-function displayGifInfo() {
-
-    var gif = $(this).attr("data-name");
-    var queryURL = "http://api.giphy.com/v1/gifs/search?q=" + gif + "&api_key=ZhBYk7Ogg5uqcY86VNUVcMn8UCUesZ9B&limit=10&rating=pg";
-    console.log("var gif " + gif);
-
-    $.ajax({
-        url: queryURL,
-        method: "GET"
-    }).then(function (response) {
-
-        // Creates a div to hold the gif
-        gifView = $("#gifs-view");
-        // make this empty to wipe the screen
-        $("#gifs-view").prepend(gifView);
-        console.log("response is below");
-        console.log(response);
+$(document).ready(function () {
 
 
-        for (var i = 0; i < response.data.length; i++) {
+    var topics = [
+        'Battlestar Galactica', 'Star Wars', 'Star Trek', 'Peter Sellers', 'Stargate', 'galaxies', 'solar system', 'TIE fighter', 'Guardians of the Galaxy'
+    ]
 
-            // varibles for static and moving gifs
-            staticGif = response.data[i].images.fixed_width_still.url;
-            movingGif = response.data[i].images.fixed_width.url;
+    console.log("GifTastic app.js loaded")
 
-
-            // Creates an element to hold the image, appends the image
-            gifView.append($("<img>").attr("src", response.data[i].images.fixed_width_still.url));
-
-            // Retrieves the Rating Data and creates an element to have the rating displayed
-            gifView.append($("<p>").text("Rating: " + response.data[i].rating));
-
-        };
-    });
-
-}
-
-function renderButtons() {
-
-    $("#gifButtons").empty();
-
-    // Loops through the array 
-    for (var j = 0; j < topics.length; j++) {
-
-        // dynamically generates buttons for each gif in the array
-        var a = $("<button>");
-        // Adds a class of gif to our button
-        a.addClass("gif");
-        // Added a data-attribute
-        a.attr("data-name", topics[j]);
-        // Provided the initial button text
-        a.text(topics[j]);
-        // Added the button to the gifButtons div
-        $("#gifButtons").append(a);
-    }
-}
-
-$("#gifs-view").on("click", function () {
-
-
-});
-
-// This function handles events where the add gif button is clicked
-$("#add-gif").on("click", function (event) {
-    event.preventDefault();
-    // This line of code will grab the input from the textbox
-    var gif = $("#gif-input").val().trim();
-
-    // The gif from the textbox is then added to our array
-    topics.push(gif);
-
+    // Append Intial buttons
     renderButtons();
+
+    $(document).on('click', '.gif_button', displayGif);
+
+
+    $(document).on('click', '.gif_container', gifActive);
+
+    // ===================================================================
+
+    // Append gif Buttons to DOM
+    function renderButtons() {
+
+        // Deletes any previous button to prevent duplicates
+        $('.gif_buttons').empty();
+
+        // Loops through the array
+        for (var i = 0; i < topics.length; i++) {
+
+            // dynamicaly generate a button for each gif in the array 
+            var gifButton = $('<button>')
+            gifButton.addClass('gif_button'); // Added a class 
+            gifButton.attr('data-name', topics[i]); // Added a data-attribute
+            gifButton.text(topics[i]); // Provided the initial button text
+            $('.gif_buttons').append(gifButton); // Added the button to the HTML
+        }
+    }
+
+    // ===================================================================
+
+    // Add new topics from the user input
+    $('#add_gif').on('click', function () {
+
+        // Grab the input from the textbox
+        var newgif = $('#gif_input').val().trim().toLowerCase();
+
+        // Add the new gif to the original list
+        topics.push(newgif);
+
+        // Add new buttons to the rendered HTML
+        renderButtons();
+
+    })
+
+    // Get gifs via AJAX call, display in HTML when buttons is clicked
+    function displayGif() {
+
+        // Deletes old gifs
+        $('#gif_images').empty();
+
+        // Collect gif name data attribute from the button, replacing any spaces
+        var gif = $(this).attr('data-name').replace(/ /g, '+');
+
+        // Create the API URL
+        var queryURL = "http://api.giphy.com/v1/gifs/search?q=" + gif + "&api_key=ZhBYk7Ogg5uqcY86VNUVcMn8UCUesZ9B&limit=10&rating=pg";
+        // console.log(queryURL);
+
+
+        // Creates AJAX call for the specific gif button being clicked
+        $.ajax({
+            url: queryURL,
+            method: 'GET'
+        }).done(function (response) {
+
+            // Loop through the JSON to collect each gif
+            for (var i = 0; i < response.data.length; i++) {
+
+                // Collect the gif gif URLs
+                var staticGif = response.data[i].images.fixed_height_still.url;
+                var movingGif = response.data[i].images.fixed_height.url;
+
+                // Collect the gif rating
+                var giphyRating = response.data[i].rating;
+
+                // Create a Div to house Gif and Rating
+                var gifDiv = $('<div>');
+                gifDiv.addClass('gif_container');
+                gifDiv.attr('data-name', "unclicked");
+
+                // Append Rating to current gif
+                var gifRating = $('<h1>');
+                gifRating.text("Rating: " + giphyRating);
+                gifDiv.append(gifRating);
+
+                // Append static gif
+                var stillGif = $('<img>');
+                stillGif.addClass('still_gif');
+                stillGif.attr("src", staticGif);
+                gifDiv.append(stillGif);
+
+                // Append Moving Gif Image
+                var currentGif = $('<img>')
+                currentGif.addClass('moving_gif');
+                currentGif.attr("src", movingGif);
+                currentGif.hide();
+                gifDiv.append(currentGif);
+
+                $('#gif_images').append(gifDiv);
+
+            }
+
+        });
+    }
+
+    // toggles between static and moving gifs
+    function gifActive() {
+
+        // determine in the gif was already clicked
+        var isClicked = $(this).attr('data-name');
+
+        // if gif is not clicked yet - Hide the static gif & display the moving gif
+        if (isClicked == "unclicked") {
+
+            var gifChildren = $(this).children();
+
+            // hide static gif
+            $(gifChildren[1]).hide();
+
+            // display moving gif
+            $(gifChildren[2]).show();
+
+            $(this).attr('data-name', "clicked");
+
+        }
+        // gif was clicked - Hide the moving gif & display the static gif
+        else {
+
+            var gifChildren = $(this).children();
+
+            // hide moving gif
+            $(gifChildren[2]).hide();
+
+            // display static gif
+            $(gifChildren[1]).show();
+
+            $(this).attr('data-name', "unclicked");
+
+        }
+
+    }
+
 });
-
-//click event when a gif is clicked
-$("#gifs-view").on("click", function () {
-
-
-});
-
-// Adding click event listeners to all elements with a class of "gif"
-$(document).on("click", ".gif", displayGifInfo);
-
-renderButtons();
-
-
-
-
-// variables as objects
-// staticGif = {
-//     url:"response.data[i].images.fixed_width_still.url",
-//     state:"static"
-// };
-
-// movingGif = {
-//     url:"response.data[i].images.fixed_width.url",
-//     state"moving"
-// };
